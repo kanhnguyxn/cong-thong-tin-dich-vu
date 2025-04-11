9875ea6420f66fb666712b8453e4772c431ed759
Xesi <hindoan10102003@gmail.com>
2025-04-11 19:38:12 +0700

    kim@nh xoa file error và loading

--- src/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <div className="w-16 h-16 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
-      <p className="mt-4 text-lg text-gray-700">Đang tải dữ liệu...</p>
-    </div>
-  );
-}
