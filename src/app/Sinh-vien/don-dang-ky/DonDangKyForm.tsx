71d563d056fa61120105dc80eec21a20dc8fd6f0
Xesi <hindoan10102003@gmail.com>
2025-04-25 01:21:19 +0700

    kim@nh tao form radio

--- src/app/Sinh-vien/don-dang-ky/DonDangKyForm.tsx
+++ src/app/Sinh-vien/don-dang-ky/DonDangKyForm.tsx
@@ -6,7 +6,7 @@ export default function DonDangKyForm() {
     {
       name: "field1",
       label: "Học kỳ",
-      type: "checkbox-group",
+      type: "radio",
       selectOptions: ["Học kỳ 1", "Học kỳ 2", "Toàn khóa"],
     },
   ];
@@ -15,7 +15,7 @@ export default function DonDangKyForm() {
     <FormMui
       inputSchema={inputSchema}
       onSubmit={(formData) => {
-        // console.log("formData", formData);
+        console.log("formData", formData);
       }}
       buttons={[{ label: "Đăng Ký", type: "submit" }]}
       //   onChange={handleChange}
