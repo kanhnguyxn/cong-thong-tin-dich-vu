a149374a0e83a09bc672711344d85940a228e9fd
Xesi <hindoan10102003@gmail.com>
2025-04-29 16:50:39 +0700

    kim@nh tao trang don dang ky

--- src/app/Sinh-vien/don-dang-ky/page.tsx
+++ src/app/Sinh-vien/don-dang-ky/page.tsx
@@ -1,12 +1,24 @@
+"use client";
 import React from "react";
 import DonDangKyForm from "./DonDangKyForm";
-// import LoaiDonForm from "./LoaiDonForm";
+import LoaiDonForm from "./LoaiDonForm";
 
 export default function ĐonangKyPage() {
+  const [maDonDangKy, setMaDonDangKy] = React.useState<string>("");
+  const [openDonDangKy, setOpenDonDangKy] = React.useState(false);
+  const handleSubmitLoaiDon = (maDon: string) => {
+    setMaDonDangKy(maDon);
+    setOpenDonDangKy(true);
+  };
   return (
-    <div className="w-full flex flex-col justify-center items-center w-[30%]">
-      {/* <LoaiDonForm /> */}
-      <DonDangKyForm maDonDangKy="DK001" />
+    <div className="flex flex-col w-full relative">
+      <div className="border-b-2 border-b-[var(--color-gray-fill)] w-full p-3 sticky top-[234px] md:top-[121px] bg-white z-40">
+        <h3 className="text-xl md:text-3xl uppercase font-bold">Đơn Đăng Ký</h3>
+      </div>
+      <div className="w-full h-full flex justify-center items-center">
+        {!openDonDangKy && <LoaiDonForm onSubmitMaDon={handleSubmitLoaiDon} />}
+        {openDonDangKy && <DonDangKyForm maDonDangKy={maDonDangKy} />}
+      </div>
     </div>
   );
 }
