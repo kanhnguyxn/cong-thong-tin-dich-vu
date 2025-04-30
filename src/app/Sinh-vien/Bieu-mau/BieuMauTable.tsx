ab719696e3ba188dd9c27a6e404da0df92671cee
Xesi <hindoan10102003@gmail.com>
2025-05-01 00:52:22 +0700

    kim@nh chinh sua style cho bieumauTable

--- src/app/Sinh-vien/Bieu-mau/BieuMauTable.tsx
+++ src/app/Sinh-vien/Bieu-mau/BieuMauTable.tsx
@@ -1,39 +1,52 @@
-'use client'
+"use client";
 
 import CustomTable from "@components/Table";
+import { text } from "stream/consumers";
 // import dữ liệu từ file Data-bieu-mau.tsx
 
 // thong tin các cột trong table
-const columns =[
-    {id: 'stt', label: 'STT', width: '5ch'},
-    {id: 'bieumau', label: 'Tên Biểu mẫu'},
-    {id: 'donVi', label: 'Đơn vị thực hiện', width: '18ch'},
-    {id: 'taixuong', label: 'Tải xuống', width: '10ch'},
-]
+const columns = [
+  { id: "stt", label: "STT", width: "5ch" },
+  { id: "bieumau", label: "Tên Biểu mẫu" },
+  { id: "donVi", label: "Đơn vị thực hiện", width: "18ch" },
+  { id: "taixuong", label: "Tải xuống", width: "10ch" },
+];
 
 const BieuMauTable = ({ data }) => {
-    // Format data to match the expected structure for CustomTable
-    const formattedData = data.map((row, index) => ({
-        stt: index + 1,
-        bieumau: row.TenBM,
-        donVi: row.TenPB,
-        taixuong: (
-            <a href={row.Lienket} download>
-                <button className='color-black'>
-                    <img src='/assets/icons/download.svg' alt="Tải xuống" width={25} height={25}/>
-                </button>
-            </a>
-        )
-    }));
+  // Format data to match the expected structure for CustomTable
+  const formattedData = data.map((row, index) => ({
+    stt: index + 1,
+    bieumau: row.TenBM,
+    donVi: row.TenPB,
+    taixuong: (
+      <a href={row.Lienket} download>
+        <button className="color-black">
+          <img
+            src="/assets/icons/download.svg"
+            alt="Tải xuống"
+            width={25}
+            height={25}
+          />
+        </button>
+      </a>
+    ),
+  }));
 
-    // Set the style to center align specific columns
-    const tableCellStyles = {
-        '&[data-column-id="stt"]': { textAlign: 'center' },
-        '&[data-column-id="donVi"]': { textAlign: 'center' },
-        '&[data-column-id="taixuong"]': { textAlign: 'center' }
-    };
+  // Set the style to center align specific columns
+  const tableCellStyles = (columnId, row) => {
+    if (columnId === "bieumau") {
+      return { textAlign: "left" };
+    }
+    return { textAlign: "center" };
+  };
 
-    return <CustomTable columns={columns} data={formattedData} tableCellStyles={tableCellStyles} />;
-}
+  return (
+    <CustomTable
+      columns={columns}
+      data={formattedData}
+      tableCellStyles={tableCellStyles}
+    />
+  );
+};
 
 export default BieuMauTable;
--- src/app/Sinh-vien/Bieu-mau/page.tsx
+++ src/app/Sinh-vien/Bieu-mau/page.tsx
@@ -3,27 +3,31 @@ import { useState } from "react";
 
 import { SearchBar } from "@components/SearchBar";
 import BieuMauTable from "./BieuMauTable";
-import dataBieuMau from "../../services/Data-bieu-mau"; 
-
+import dataBieuMau from "../../services/Data-bieu-mau";
 
 export default function BieuMauPage() {
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([]);
+  const onSearch = (query: string) => {
+    if (!query || query.trim() === "") return setData(dataBieuMau); // Nếu không có từ khóa tìm kiếm, hiển thị tất cả dữ liệu
+    const filteredData = dataBieuMau.filter((item) =>
+      item.TenBM.toLowerCase().includes(query.toLowerCase())
+    );
+    setData(filteredData);
+    console.log(filteredData, query);
+  };
 
-    const onSearch = (query:string) => {
-        if(!query|| query.trim() === "") return setData(dataBieuMau); // Nếu không có từ khóa tìm kiếm, hiển thị tất cả dữ liệu
-        const filteredData = dataBieuMau.filter((item) => item.TenBM.toLowerCase().includes(query.toLowerCase()));
-        setData(filteredData);
-        console.log(filteredData, query);
-    }
-
-    return(
-        <div className="flex flex-col w-full items-center">
-            <div className="border-b-2 border-b-[var(--color-gray-fill)] w-full p-3 flex justify-between sticky  top-[234px] md:top-[121px] bg-white z-40">
-                <h3 className='text-xl md:text-3xl uppercase font-bold'>Tra cứu biểu mẫu</h3>
-                <SearchBar onSearch={onSearch}></SearchBar>
-            </div>
-            <BieuMauTable data={data}/>
-        </div>
-    )
+  return (
+    <div className="flex flex-col w-full items-center">
+      <div className="border-b-2 border-b-[var(--color-gray-fill)] w-full p-3 flex justify-between sticky  top-[234px] md:top-[121px] bg-white z-40">
+        <h3 className="text-xl md:text-3xl uppercase font-bold">
+          Tra cứu biểu mẫu
+        </h3>
+        <SearchBar onSearch={onSearch}></SearchBar>
+      </div>
+      <div className="w-full px-4 md:max-w-[90%] lg:max-w-[80%]">
+        <BieuMauTable data={data} />
+      </div>
+    </div>
+  );
 }
