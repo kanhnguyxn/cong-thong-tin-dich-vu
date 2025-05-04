d17f9c4d5989d8ba3b2562986e586a18203a12c5
Xesi <hindoan10102003@gmail.com>
2025-05-05 01:27:25 +0700

    kim@nh chinh trang dang nhap

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -3,23 +3,28 @@
 export const loginRequest = async (
   usename: string,
   password: string
-): Promise<{ accessToken: string; userType: 'student' | 'manager' | 'staff' }> => {
+): Promise<{ accessToken: string; userType: 'student' | 'manager' | 'staff' , refreshToken:string}> => {
   // Giả lập API backend
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (usename === 'admin' && password === '123456') {
         resolve({
           accessToken: 'mock_access_token_abc123',
+          refreshToken: 'mock_refresh_token_abc123',
           userType: 'manager', // manager
         });
       } else if (usename === 'student' && password === '123456') {
         resolve({
           accessToken: 'mock_access_token_xyz789',
+          refreshToken: 'mock_refresh_token_abc123',
+          
           userType: 'student', // student
         });
       } else if (usename === 'staff' && password === '123456') {
         resolve({
           accessToken: 'mock_access_token_qwe456',
+          refreshToken: 'mock_refresh_token_abc123',
+
           userType: 'staff', // staff
         });
       } else {
