da2e87cf5185ebeaef23c9cb3a74129e5b63f530
Xesi <hindoan10102003@gmail.com>
2025-05-06 17:00:23 +0700

    kim@nh chinh sua phan header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,32 +1,49 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Logo from './Logo';
-import AvatarMenu from './Avatar';
-import Navbar from './Navbar';
-
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Logo from "./Logo";
+import AvatarMenu from "./Avatar";
+import Navbar from "./Navbar";
+import "@styles/globals.css";
+import "@styles/colors.css";
 
 const infoHeader = {
-  logo: '/assets/icons/logo_truong.svg', 
-  school: 'Đại học Đà Nẵng',
-  unit:'Trường Đại học Kinh Tế',
-  system:'Cổng thông tin dich vụ',
-  background_image:"/assets/images/background_header.svg",
-  avatarUrl :'/assets/icons/avatar.svg',
-}
+  logo: "/assets/icons/logo_truong.svg",
+  unit: "Trường Đại học Kinh Tế - Đại học Đà Nẵng",
+  system: "Cổng thông tin - dich vụ",
+  background_image: "/assets/images/background_header.svg",
+  avatarUrl: "/assets/icons/avatar.svg",
+};
 
-const Header = ({isLoggedIn = false}) => {
+const Header = (userType: { userType: string }) => {
   return (
-    <AppBar position="static" sx={{backgroundColor: 'transparent', boxShadow:'none'}} >
-        <Toolbar disableGutters className='flex flex-col text-sm md:text-md lg:text-lg xl:text-xl'>
-          <Box className='flex w-full justify-between items-center py-3 md:py-0' sx={isLoggedIn ? { backgroundImage: `url(${infoHeader.background_image})`, backgroundSize: 'cover', backgroundRepeat:'no-repeat'} : {backgroundColor: 'transparent'}}>
-          <Logo title={isLoggedIn? infoHeader.system : infoHeader.school} subTitle={infoHeader.unit} logo={infoHeader.logo} 
-          className={`${isLoggedIn ? 'text-md md:text-lg lg:text-2xl' : ''}`}/>
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "transparent", boxShadow: "none" }}
+    >
+      <Toolbar
+        disableGutters
+        className="flex flex-col text-sm md:text-md lg:text-lg xl:text-xl"
+      >
+        <Box
+          className="flex w-full justify-between items-center py-3 md:py-0"
+          sx={{
+            backgroundImage: `url(${infoHeader.background_image})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <Logo
+            title={infoHeader.system}
+            subTitle={infoHeader.unit}
+            logo={infoHeader.logo}
+            className="text-md md:text-lg lg:text-2xl text-[var(--color-orange)]"
+          />
           {/* nếu chưa đăng nhập sẽ không hiện avtar */}
-          {isLoggedIn && <AvatarMenu avatarUrl={infoHeader.avatarUrl}/>}
-          </Box>
-          {isLoggedIn && <Navbar/>}
-        </Toolbar>
+          <AvatarMenu avatarUrl={infoHeader.avatarUrl} />
+        </Box>
+        <Navbar />
+      </Toolbar>
     </AppBar>
   );
 };
