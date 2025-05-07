6c7aecec24fd9353d4a108b4f701d8dd9d965ec4
Xesi <hindoan10102003@gmail.com>
2025-05-07 18:40:23 +0700

    kim@nh viet api change pasword

---
+++ src/app/api/auth/changePassword.tsx
@@ -0,0 +1,44 @@
+import requestWithAuth from "@utils/requestWithAuth";
+
+export default function changePassword({
+  oldPassword,
+  newPassword,
+  confirmNewPassword,
+}: {
+  oldPassword: string;
+  newPassword: string;
+  confirmNewPassword: string;
+}) {
+  try {
+    const resData = requestWithAuth({
+      input: "auth/change-password",
+      init: {
+        method: "POST",
+        body: {
+          oldPassword: oldPassword,
+          newPassword: newPassword,
+          confirmNewPassword: confirmNewPassword,
+        },
+      },
+    });
+    switch (resData.status) {
+      case 200:
+        return true;
+      case 400:
+        console.log("Mật khẩu không hợp lệ");
+        return false;
+      case 401:
+        console.log("Phiên đăng nhập đã hết hạn, vui lòng đăng nhập lại!");
+        return false;
+      case 500:
+        console.log("Có lỗi xảy ra, vui lòng thử lại sau");
+        return false;
+      default:
+        console.log("Có lỗi xảy ra, vui lòng thử lại sau");
+        return false;
+    }
+  } catch (error) {
+    console.error("Lỗi khi thay đổi mật khẩu:", error);
+    return false;
+  }
+}
