9ca4d7ab694fd848727441649a2067432001e5ff
Xesi <hindoan10102003@gmail.com>
2025-05-08 18:06:55 +0700

    kim@nh them API reset password va logic

--- src/app/api/auth/resetPasswordAPI.tsx
+++ src/app/api/auth/resetPasswordAPI.tsx
@@ -1,31 +1,23 @@
 const API_BASE_URL = "https://ea8a-123-19-224-121.ngrok-free.app/api";
 
-export const loginRequest = async (username: string, password: string) => {
-  const res = await fetch(`${API_BASE_URL}/auth/login`, {
+export async function resetPasswordRequest(email: string, password: string) {
+  const res = await fetch(`${API_BASE_URL}/auth/reset-password`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ username, password }),
+    body: JSON.stringify({ email: email, password: password }),
   });
-
- 
-
   switch (res.status) {
     case 200:
-      const resData = await res.json();
-      return resData;
-
+      return true;
     case 400:
       throw new Error("Dữ liệu không hợp lệ");
-
     case 401:
-      throw new Error("Đăng nhập thất bại, hãy thử lại!");
-
+      throw new Error("Email không tồn tại trong hệ thống");
     case 500:
       throw new Error("Đã có lỗi hệ thống, vui lòng thử lại sau");
-
     default:
       throw new Error("Đã xảy ra lỗi không xác định");
   }
-};
+}
