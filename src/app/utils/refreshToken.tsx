ccc0e4177b06ce6ac3853144524fed974280dd81
Xesi <hindoan10102003@gmail.com>
2025-05-11 13:11:09 +0700

    kim@nh chinh sua file refreshToken

--- src/app/utils/refreshToken.tsx
+++ src/app/utils/refreshToken.tsx
@@ -1,13 +1,15 @@
-const API_BASE_URL = "https://ea8a-123-19-224-121.ngrok-free.app/api";
-
-export async function refreshToken({ refreshToken }: { refreshToken: string }) {
+const API_BASE_URL = "https://3522-123-19-224-121.ngrok-free.app/api";
+import getToken from "./getToken";
+export async function refreshToken() {
+  const { access, refresh } = getToken();
   try {
     const res = await fetch(`${API_BASE_URL}/auth/refresh-token`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${access}`,
       },
-      body: JSON.stringify({ refreshToken: refreshToken }),
+      body: JSON.stringify({ refreshToken: refresh }),
     });
 
     if (!res.ok) {
@@ -15,7 +17,7 @@ export async function refreshToken({ refreshToken }: { refreshToken: string }) {
     }
     const resData = await res.json();
     // luwu lai access token
-    document.cookie = `access=${resData.accessToken}; path=/; max-age=7200`;
+    document.cookie = `access=${resData.accessToken}; path=/; max-age=10800`;
     return true;
   } catch (error) {
     console.error("Lỗi khi làm mới token:", error);
