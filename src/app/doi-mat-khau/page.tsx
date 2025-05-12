073c8bdd20e0dccd2086ce512fe4ee5d7af2a449
Xesi <hindoan10102003@gmail.com>
2025-05-12 12:30:20 +0700

    kim@nh chinh sua trang doi mat khau

--- src/app/doi-mat-khau/page.tsx
+++ src/app/doi-mat-khau/page.tsx
@@ -5,7 +5,7 @@ export default function ChangePasswordPage() {
     <div className="flex flex-col w-full">
       <div className="border-b-2 border-b-[var(--color-gray-fill)] w-full p-3 sticky  top-[234px] md:top-[121px] bg-white z-40">
         <h3 className="text-xl md:text-3xl uppercase font-bold">
-          Doi mat khau
+          Đổi mật khẩu
         </h3>
       </div>
       <div className="flex flex-col w-full items-center h-full justify-center">
