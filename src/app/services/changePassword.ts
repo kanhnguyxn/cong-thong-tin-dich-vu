e622b812b8e5a6466b207a71fd63744f628cd103
Xesi <hindoan10102003@gmail.com>
2025-05-12 12:38:10 +0700

    xoa phan services

--- src/app/services/changePassword.ts
+++
@@ -1,8 +0,0 @@
-export default function ChangePasswordService(curentPassword) {
-    if (curentPassword !== 'password'){
-        return 'ĐỔI MẬT KHẨU THẤT BẠI';
-    }
-    else{
-        return 'ĐỔI MẬT KHẨU THÀNH CÔNG';
-    }
-}
