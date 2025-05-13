c55118e0dc52853cca10dd13e68a7350a9153ddb
Xesi <hindoan10102003@gmail.com>
2025-05-14 00:33:00 +0700

    kim@nh chinh sua store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlide';
 
-const store = configureStore({
-  reducer: {
-    user: authReducer,
-  },
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
-export default store;
+export const makeStore =()=>{
+  return configureStore({
+    reducer: {
+      auth: authReducer,
+    },
+  })
+}
+// lay ra store
+export type AppStore = ReturnType<typeof makeStore>;
+// lay state
+export type RootState = ReturnType<AppStore['getState']>;
+// thuc hien cac action
+export type AppDispatch = AppStore['dispatch'];
