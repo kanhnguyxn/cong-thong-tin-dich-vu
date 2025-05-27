98eb958183c35662535256cb842e3fbd2e2d1e5a
Xesi <hindoan10102003@gmail.com>
2025-05-28 00:32:29 +0700

    kim@nh them slice cho gioi thieu

--- src/apis/sinhVien/getGioiThieu.tsx
+++ src/apis/sinhVien/getGioiThieu.tsx
@@ -8,7 +8,7 @@ export async function getGioiThieu() {
       method: methods.GET,
     });
     GetStatusCode(resData.statusCode);
-    // console.log("resData", resData);
+    // console.log("resData", resData.data);
     return resData.data;
   } catch (error) {
     throw new Error(error || "Lỗi khi lấy giới thiệu");
