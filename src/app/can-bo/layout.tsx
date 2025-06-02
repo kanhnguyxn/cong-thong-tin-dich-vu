118437199cb3c9875a90eb0e8344b1fc9a102fab
kimanh <hindoan10102003@gmail.com>
2025-06-02 23:59:45 +0700

    kim@nh them form modal

--- src/app/can-bo/layout.tsx
+++ src/app/can-bo/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "@components/Footer";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen max-w-full">
-      <div className="sticky top-0 z-50 w-full">
+      <div className="sticky top-0 z-10 w-full">
         <Header />
       </div>
       <main className="flex-1 flex w-full">{children}</main>
