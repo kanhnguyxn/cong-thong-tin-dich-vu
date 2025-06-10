80b1abce57ffcc3e96d9205509fd5f2d3fefa78a
Xesi <hindoan10102003@gmail.com>
2025-06-11 00:55:13 +0700

    kim@nh chinh sua style cho don dang ky

--- src/app/can-bo/don-dang-ky/DonDangKyHeader.tsx
+++ src/app/can-bo/don-dang-ky/DonDangKyHeader.tsx
@@ -10,67 +10,75 @@ export default function DonDangKyHeader({}) {
   const isAdd = pathName[pathName.length - 1] === "tao-moi";
 
   return (
-    <div className="border-b-2 border-b-[var(--color-gray-fill)] w-full p-3 flex justify-between sticky  top-[234px] md:top-[121px] bg-white z-40">
+    <div className="border-b-2 border-b-[var(--color-gray-fill)] w-full p-3 grid grid-cols-6 sticky  top-[234px] md:top-[121px] bg-white z-40">
       <h3 className="text-xl md:text-3xl uppercase font-bold">Đơn Đăng Ký</h3>
-      <div className="flex flex-end gap-2">
-        <CustomButton
-          label={isAdd ? "Xác nhận" : "Thêm"}
-          sx={{ width: "100%", backgroundColor: "var(--color-blue)" }}
-          onClick={() => {
-            if (!isAdd) {
-              showModal({
-                title: "Thêm đơn đăng ký",
-                icon: "warning",
-                type: "form",
-                inputs: [
-                  {
-                    name: "loaiDonDangKy",
-                    label: "",
-                    type: "radio-group",
-                    required: true,
-                    selectOptions: [
-                      { display: "Liên kết", value: "link" },
-                      { display: "Đơn mới", value: "form" },
-                    ],
-                  },
-                ],
-              }).then((res: any) => {
-                if (res.data) {
-                  if (res.data.loaiDonDangKy === "form") {
-                    router.push("/can-bo/don-dang-ky/tao-moi");
-                  } else {
-                    showModal({
-                      title: "Liên kết",
-                      type: "form",
-                      formOrientation: "vertical",
-                      handleAsyncSubmit: async (data: any) => {
-                        // Handle the form submission logic here
-                        console.log("Submitted data:", data);
-                        return;
-                      },
-                      inputs: [
-                        {
-                          name: "link",
-                          label: "Thêm liên kết đơn đăng ký",
-                          type: "text",
-                          required: true,
-                          placeholder: "Nhập liên kết",
-                        },
-                        {
-                          name: "tendonDangKy",
-                          label: "Tên đơn",
-                          type: "text",
-                          required: true,
-                          placeholder: "Nhập tên đơn",
-                        },
+      <div
+        className={`col-end-7 ${
+          !isAdd ? "" : "w-[50%] place-self-end"
+        } flex flex-row gap-2`}
+      >
+        {!isAdd && (
+          <CustomButton
+            label={"Thêm"}
+            sx={{ width: "100%", backgroundColor: "var(--color-blue)" }}
+            onClick={() => {
+              if (!isAdd) {
+                showModal({
+                  title: "Thêm đơn đăng ký",
+                  icon: "warning",
+                  type: "form",
+                  classContainer: "w-[25%]",
+                  inputs: [
+                    {
+                      name: "loaiDonDangKy",
+                      label: "",
+                      type: "radio-group",
+                      required: true,
+                      selectOptions: [
+                        { display: "Liên kết", value: "link" },
+                        { display: "Đơn mới", value: "form" },
                       ],
-                    });
+                    },
+                  ],
+                }).then((res: any) => {
+                  if (res.data) {
+                    if (res.data.loaiDonDangKy === "form") {
+                      router.push("/can-bo/don-dang-ky/tao-moi");
+                    } else {
+                      showModal({
+                        title: "Liên kết",
+                        type: "form",
+                        formOrientation: "vertical",
+                        handleAsyncSubmit: async (data: any) => {
+                          // Handle the form submission logic here
+                          console.log("Submitted data:", data);
+                          return;
+                        },
+                        inputs: [
+                          {
+                            name: "link",
+                            label: "Thêm liên kết đơn đăng ký",
+                            type: "text",
+                            required: true,
+                            placeholder: "Nhập liên kết",
+                          },
+                          {
+                            name: "tendonDangKy",
+                            label: "Tên đơn",
+                            type: "text",
+                            required: true,
+                            placeholder: "Nhập tên đơn",
+                          },
+                        ],
+                      });
+                    }
                   }
-                }
-              });
-            }
-          }}
-        />
+                });
+              }
+            }}
+          />
+        )}
+
         <CustomButton
           label={isAdd ? "Hủy" : "Xóa"}
           sx={{ width: "100%", backgroundColor: "var(--color-blue)" }}
